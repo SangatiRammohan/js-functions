c59798e2ce236c66f039e8ff8514ae4eab4c2f3c
sangatirammohan <sangatirammohan01@gmail.com>
2024-11-02 14:38:33 +0530

    functions are created

---
+++ fun.js
@@ -0,0 +1,16 @@
+/*  create 2 functions with studentName and anotherFunction with their surname and execute the code and
+ write the execution of function behind the scenes  and how FEC creates in call stack and how it vanishes there
+*/
+let num3=90
+function ramMohan(){
+let num=30
+console.log(num+ num3)
+}
+ramMohan()
+
+function sanGati(){
+var digit=40
+var num2=60
+console.log(digit + num2)
+}
+sanGati()
